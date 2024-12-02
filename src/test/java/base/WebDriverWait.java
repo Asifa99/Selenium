b5ecc64f14de6e3c649e37a7ed7677b7db60fee2
Asifa Iqbal <asifaiqbal99@gmail.com>
2024-12-02 18:55:02 +0500

    Selenium tutorial

---
+++ src/test/java/base/WebDriverWait.java
@@ -0,0 +1,10 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+
+public class WebDriverWait {
+    public WebDriverWait(WebDriver driver, Duration duration) {
+    }
+}
