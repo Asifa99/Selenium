da0f196e0a9719e54bef2f05062f8996b9d797ce
Asifa Iqbal <asifaiqbal99@gmail.com>
2024-12-02 19:01:31 +0500

    Seleniuml video

--- src/test/java/base/Youtubevideo.java
+++ src/test/java/base/Youtubevideo.java
@@ -20,11 +20,8 @@ public class Youtubevideo {
         driver.get("https://www.youtube.com/");
         System.out.println(driver.getTitle());
     }
-
-    // Method to search for a video
     public void searchVideo() {
         try {
-            // Create an instance of WebDriverWait with a 10-second timeout
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
             // Wait until the search box is visible and type the search term
