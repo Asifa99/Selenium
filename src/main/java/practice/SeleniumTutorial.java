65e814740b061fc390fb74a4dc44fe5c5dfd2f98
Asifa Iqbal <asifaiqbal99@gmail.com>
2024-12-04 10:33:44 +0500

    added dropdown

--- src/main/java/practice/SeleniumTutorial.java
+++ src/main/java/practice/SeleniumTutorial.java
@@ -1,9 +1,12 @@
 package practice;
 
 import org.openqa.selenium.By;
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.Properties;
 import java.io.InputStream;
@@ -13,6 +16,12 @@ public class SeleniumTutorial {
     private static String searchboxxpath;
     private static String searchtext;
     private static String firstvideoxpath;
+    private static String filterbuttonxpath;
+    private static String videofilterbutton;
+    private static String filtermin;
+    private static String sortbydropdown;
+    private static String uploaddateoption;
+
 
     public static void main(String[] args) throws InterruptedException {
         Properties properties = new Properties();
@@ -33,6 +42,11 @@ public class SeleniumTutorial {
             searchboxxpath = properties.getProperty("search.box.xpath");
             searchtext = properties.getProperty("search.text");
             firstvideoxpath = properties.getProperty("first.video.xpath");
+            filterbuttonxpath=properties.getProperty("filter.button.xpath");
+            videofilterbutton=properties.getProperty("video.filter.button");
+            filtermin=properties.getProperty("filter.min");
+            sortbydropdown=properties.getProperty("sort.by.dropdown");
+            uploaddateoption=properties.getProperty("upload.date.option");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,22 +65,37 @@ public class SeleniumTutorial {
         searchBox.submit();
         Thread.sleep(2000);
 
-        WebElement filterBtn = driver.findElement(By.xpath("//div[@id='filter-button']"));
+        WebElement filterBtn = driver.findElement(By.xpath(filterbuttonxpath));
         filterBtn.click();
         Thread.sleep(2000);
 
-        WebElement videoFilter = driver.findElement(By.xpath("//div[@id='label' and @title='Search for Video']"));
+        WebElement videoFilter = driver.findElement(By.xpath(videofilterbutton));
         videoFilter.click();
         Thread.sleep(2000);
 
         filterBtn.click();
         Thread.sleep(2000);
 
-        WebElement filterMin=driver.findElement(By.xpath("//div[@id='label' and @title='Search for Over 20 minutes']"));
+        WebElement filterMin=driver.findElement(By.xpath(filtermin));
         filterMin.click();
         Thread.sleep(2000);
 
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//
+//        // Interact with Sort by Dropdown
+//        filterBtn.click();
+//        Thread.sleep(2000);
+//
+//        WebElement sortByDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(sortbydropdown)));
+//        sortByDropdown.click();
+//
+//        // Select "Upload date" from the dropdown
+//        WebElement uploadDateOption = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(uploaddateoption)));
+//        uploadDateOption.click();
+
         WebElement firstVideo = driver.findElement(By.xpath(firstvideoxpath));
         firstVideo.click();
+
+
     }
 }
